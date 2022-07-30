f7b326459ff50369d91ffc141375c535d1dd1ebf
jay-choe <jay0764@naver.com>
2022-07-30 21:15:54 +0900

    Initial commit And Add nio selector server demo

---
+++ niotest/selector/SelectorServer.java
@@ -0,0 +1,73 @@
+package selector;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Set;
+import selector.common.Config;
+
+public class SelectorServer {
+
+    private Selector selector;
+
+    public SelectorServer() {
+        try (Selector selector = Selector.open()) {
+            this.selector = selector;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listen() throws IOException {
+        System.out.println("Server is listening to: " + Config.PORT);
+
+        while (true) {
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = keys.iterator();
+
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                if (key.isAcceptable()) {
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = channel.accept();
+                    System.out.println("=======Accepted========");
+                    socketChannel.write(ByteBuffer.wrap("Hello".getBytes(StandardCharsets.UTF_8)));
+
+                } else if (key.isReadable()) {
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    ByteBuffer buffer = ByteBuffer.allocate(Config.BUFFER_SIZE);
+                    channel.read(buffer);
+                    byte[] data = buffer.array();
+                    String msg = new String(data).trim();
+                    buffer.clear();
+                }
+            }
+        }
+    }
+
+    private void init(int port) {
+        assert port > 0 && String.valueOf(port).length() > 3;
+
+        try (ServerSocketChannel channel = ServerSocketChannel.open()) {
+            channel.socket().bind(new InetSocketAddress(port));
+            // for Non-blocking I/O
+            channel.configureBlocking(false);
+            channel.register(selector, SelectionKey.OP_ACCEPT | SelectionKey.OP_CONNECT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        SelectorServer server = new SelectorServer();
+        server.init(Config.PORT);
+        server.listen();
+    }
+}
---
+++ niotest/selector/common/Config.java
@@ -0,0 +1,6 @@
+package selector.common;
+
+public class Config {
+    public static int PORT = 9000;
+    public static int BUFFER_SIZE = 1024;
+}
